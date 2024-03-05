c4818042f5b5d917c948585f4b9fc57abdc7e1be
Ramchandra Mulik <rajumulik51@gmail.com>
2024-03-05 11:29:08 +0530

    utils code discuss

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("server is live on port :", process.env.PORT);
+    });
+    app.on("error", (error) => {
+      console.log("ERROR :- ", error);
+      throw error;
+    });
+
+    app.get("", (req, res) => {
+      res.send("hello world sakec");
+    });
+  })
+  .catch((err) => {
+    console.log("databse connection ERROR !!!:-", err);
+  });
 
 /*
 import  express  from "express";
